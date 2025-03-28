37453d41b270e82020944aa3cf2a15e80597b08d
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-03-28 17:58:27 +0100

    typo fixed

--- One Weekend/RayTracingOneWeekend/hittable.h
+++ One Weekend/RayTracingOneWeekend/hittable.h
@@ -11,9 +11,9 @@ public:
 	double t;
 	bool front_face;
 
-	void set_face_normal(const ray& r, const vec3& outward_nomrmal) {
-		front_face = dot(r.direction(), outward_nomrmal) < 0;
-		normal = front_face ? outward_nomrmal : -outward_nomrmal;
+	void set_face_normal(const ray& r, const vec3& outward_normal) {
+		front_face = dot(r.direction(), outward_normal) < 0;
+		normal = front_face ? outward_normal : -outward_normal;
 	}
 };
 
