d3d5625f013705ab693fd38eec799b1e32d8c09f
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-04-14 19:39:11 +0200

    Settings Options

--- One Weekend/RayTracingOneWeekend/Main.cpp
+++ One Weekend/RayTracingOneWeekend/Main.cpp
@@ -14,12 +14,24 @@ int main() {
     world.add(std::make_shared<sphere>(vec3(0, 100.5, -1), 100));
 
     camera cam;
-
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1920;
-    cam.samples_per_pixel = 100;
-    cam.max_depth = 50;
-
+    
+    
+    bool is_test_settings = true;
+    if (is_test_settings) {
+        // Test Settings
+        cam.aspect_ratio = 16.0 / 9.0;
+        cam.image_width = 500;
+        cam.samples_per_pixel = 100;
+        cam.max_depth = 50;
+    }
+    else {
+        // Relase Settings
+        cam.aspect_ratio = 16.0 / 9.0;
+        cam.image_width = 1920;
+        cam.samples_per_pixel = 1000;
+        cam.max_depth = 50;
+    }
+    
     cam.render(world);
     return 0;
 }
