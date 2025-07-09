c782d1c92865f3bfceb542eeba44a0f98f98d435
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-07-09 22:50:44 +0200

    add image quality presets

--- One Weekend/RayTracingOneWeekend/Main.cpp
+++ One Weekend/RayTracingOneWeekend/Main.cpp
@@ -4,34 +4,62 @@
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "presets.h"
+
+//-----------------------------------------------------------------------------
+//                          Ray Tracing Tutorial - Main
+//
+//  Note this is a tutorial prpject based on the infamous Ray Tracing In a 
+//  Weekend series. Thus all of this code is not created by me. 
+// 
+//  This is the entry point of the ray tracer.
+//-----------------------------------------------------------------------------
 
 
 int main() {
-
+    // Scene setup and fill up with sphere geometry
     hittable_list world;
-
     world.add(std::make_shared<sphere>(vec3(0, 0, -1), 0.5));
     world.add(std::make_shared<sphere>(vec3(0, 100.5, -1), 100));
 
+    // Camera (shooting rays from it)
     camera cam;
     
-    
-    bool is_test_settings = true;
-    if (is_test_settings) {
-        // Test Settings
-        cam.aspect_ratio = 16.0 / 9.0;
-        cam.image_width = 500;
-        cam.samples_per_pixel = 100;
-        cam.max_depth = 50;
+    // Image Presets
+    enum Presets preset = PERFORMANCE;
+    switch (preset) {
+        case POOR:
+            cam.aspect_ratio = 16.0 / 9.0;
+            cam.image_width = 500;
+            cam.samples_per_pixel = 100;
+            cam.max_depth = 30;
+            break;
+        case PERFORMANCE:
+            cam.aspect_ratio = 16.0 / 9.0;
+            cam.image_width = 1000;
+            cam.samples_per_pixel = 100;
+            cam.max_depth = 40;
+            break;
+        case BALANCED:
+            cam.aspect_ratio = 16.0 / 9.0;
+            cam.image_width = 1920;
+            cam.samples_per_pixel = 500;
+            cam.max_depth = 50;
+            break;
+        case BEAUTY:
+            cam.aspect_ratio = 16.0 / 9.0;
+            cam.image_width = 1920;
+            cam.samples_per_pixel = 1000;
+            cam.max_depth = 50;
+            break;
+        case FIDELITY:
+            cam.aspect_ratio = 16.0 / 9.0;
+            cam.image_width = 3840;
+            cam.samples_per_pixel = 1000;
+            cam.max_depth = 100;
     }
-    else {
-        // Relase Settings
-        cam.aspect_ratio = 16.0 / 9.0;
-        cam.image_width = 1920;
-        cam.samples_per_pixel = 1000;
-        cam.max_depth = 50;
-    }
     
+    // Render the scene
     cam.render(world);
     return 0;
 }
---
+++ One Weekend/RayTracingOneWeekend/presets.h
@@ -0,0 +1,13 @@
+#ifndef PRESET_H
+#define PRESET_H
+
+enum Presets {
+	POOR,					// Mainly for test (low resolution, 100 samples, 30 depth)
+	PERFORMANCE,			// Mainly for test (higher resolution, 100 samples, 40 depth)
+	BALANCED,				// Good Results (FULL HD, 500 samples, 50 detph)
+	BEAUTY,					// Good Quality Image (FULL HD, 1000 samples, 50 depth)
+	FIDELITY				// ABSOLUTE CINEMA (4K, 1000 samples, 100 depth)
+
+};
+
+#endif
