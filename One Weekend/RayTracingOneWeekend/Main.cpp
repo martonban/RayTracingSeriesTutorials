7e035b422579c6e206b6390d122196444723e7d1
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-07-20 17:28:09 +0200

    Metal DONE

--- One Weekend/RayTracingOneWeekend/Main.cpp
+++ One Weekend/RayTracingOneWeekend/Main.cpp
@@ -22,8 +22,8 @@ int main() {
     hittable_list world;
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(std::make_shared<sphere>(vec3(0.0, 100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<sphere>(vec3(0.0, 0.0, -1.2), 0.5, material_center));
@@ -34,7 +34,7 @@ int main() {
     camera cam;
     
     // Image Presets
-    enum Presets preset = FIDELITY;
+    enum Presets preset = BALANCED;
     switch (preset) {
         case POOR:
             cam.aspect_ratio = 16.0 / 9.0;
